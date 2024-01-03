e2de148aaae57e77fb6c08bd0de28e0c69c8c4e0
Your Name <you@example.com>
2024-01-03 21:52:46 +0500

    initial commit

--- connect.js
+++ connect.js
@@ -1,13 +1,15 @@
 import mysql from 'mysql';
 
 export const db = mysql.createConnection({
-    host: "localhost",
+    connectionLimit: 10,
+    host: "127.0.0.1",
     user: "root",
     password: "9901",
-    database: "social"
+    database: "social",
+    port:"3306"
 })
 
 db.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-  });
+});
--- routes/relationshipsRoute.js
+++ routes/relationshipsRoute.js
@@ -1,6 +1,8 @@
 import experss from 'express'
 import {getReletionships, addReletionship, deleteReletionship} from '../controlles/relationshipsCont.js'
+
 const router = experss.Router()
+
 router.get("/", getReletionships)
 router.post("/", addReletionship)
 router.delete("/", deleteReletionship)
